605468167eca9adcc9cc0e9f24da23063d2aa046
jxyu-JLU <50972352+jxyu-JLU@users.noreply.github.com>
2022-12-11 10:29:06 +0800

    Add files via upload

---
+++ 3Dearth/Assets/pen_data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class pen_data : MonoBehaviour
+{
+    [DllImport("mydll_position.dll", EntryPoint = "EnumMyHid", SetLastError = true)]
+    public static extern byte EnumMyHid();
+
+    void Start()
+    {
+        byte r = EnumMyHid();
+        Debug.Log("r = " + r.ToString());
+    }
+}
